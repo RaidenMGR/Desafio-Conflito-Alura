331db101937cbcffb77d6dc379a3569ac1d26802
Gabriel Oliveira Silva <gabrieloliveira20003@gmail.com>
2025-04-03 14:50:59 -0300

    mudando denovo o jogo de pontuação

--- semana.js
+++ semana.js
@@ -24,13 +24,13 @@ if (numero < 0) {
 // Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
 let pontuacao = 0;
 let resposta1 = prompt('a girafa é de que cor?');
-let resposta2 = prompt('o urso polar é de que cor?');
+let resposta2 = prompt('o tigre é de que cor?');
 if (resposta1 == amarela) {
    pontuacao + 50;
 } else {
    pontuacao + 0;
 }
-if (resposta2 == branco) {
+if (resposta2 == laranja) {
    pontuacao + 50;
 } else {
    pontuacao + 0;
